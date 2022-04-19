71902ed362dafed7909a6f08f3b8a9a35fe0eac6
Lorenzo Minto <lminto@brave.com>
2022-04-19 18:39:58 +0200

    Implement generic schema for BraveFederated data stores

--- components/brave_ads/browser/ads_service_impl.h
+++ components/brave_ads/browser/ads_service_impl.h
@@ -49,9 +49,6 @@ class SequencedTaskRunner;
 }  // namespace base
 
 namespace brave_federated {
-class AdNotificationTimingDataStore;
-struct AdNotificationTimingTaskLog;
-template <class T, class U>
 class AsyncDataStore;
 }  // namespace brave_federated
 
@@ -90,10 +87,7 @@ class AdsServiceImpl : public AdsService,
 #endif
       history::HistoryService* history_service,
       brave_rewards::RewardsService* rewards_service,
-      brave_federated::AsyncDataStore<
-          brave_federated::AdNotificationTimingDataStore,
-          brave_federated::AdNotificationTimingTaskLog>*
-          ad_notification_timing_data_store);
+      brave_federated::AsyncDataStore* ad_notification_timing_data_store);
   ~AdsServiceImpl() override;
 
   AdsServiceImpl(const AdsServiceImpl&) = delete;
@@ -431,7 +425,7 @@ class AdsServiceImpl : public AdsService,
                       const ads::mojom::P2AEventType type,
                       const std::string& value) override;
 
-  void LogTrainingInstance(const brave_federated::mojom::TrainingInstancePtr
+  void LogTrainingInstance(brave_federated::mojom::TrainingInstancePtr
                                training_instance) override;
   void OnLogTrainingInstance(bool success);
 
@@ -526,10 +520,8 @@ class AdsServiceImpl : public AdsService,
   raw_ptr<brave_rewards::RewardsService> rewards_service_{
       nullptr};  // NOT OWNED
 
-  raw_ptr<brave_federated::AsyncDataStore<
-      brave_federated::AdNotificationTimingDataStore,
-      brave_federated::AdNotificationTimingTaskLog>>
-      ad_notification_timing_data_store_ = nullptr;  // NOT OWNED
+  raw_ptr<brave_federated::AsyncDataStore> ad_notification_timing_data_store_ =
+      nullptr;  // NOT OWNED
 
   mojo::AssociatedReceiver<bat_ads::mojom::BatAdsClient>
       bat_ads_client_receiver_;
